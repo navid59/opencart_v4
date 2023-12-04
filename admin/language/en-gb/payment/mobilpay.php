c9239e9a512253fd820bf7fd62bf958df38642cf
Navid <torabazary@yahoo.com>
2023-12-04 19:07:23 +0200

    Admin aproxamativ workd

--- admin/language/en-gb/payment/mobilpay.php
+++ admin/language/en-gb/payment/mobilpay.php
@@ -1,36 +1,38 @@
 <?php
 // Heading
-$_['heading_title']      = 'Netopia mobilPay - Card payment';
+$_['heading_title']     = 'Netopia mobilPay - Card payment';                    // OK
 
 // Text 
-$_['text_extension']	 = 'Extensions';
-$_['text_payment']       = 'Payment';
-$_['text_success']       = 'Success: You have modified Netopia mobilPay account details!';
-$_['text_edit']                     = 'Edit Netopia mobilPay';
-$_['text_mobilpay']        = '<a onclick="window.open(\'http://www.mobilpay.ro\');"><img src="view/image/payment/mobilpay.gif" alt="Netopia mobilPay" title="Netopia mobilPay" style="border: 1px solid #EEEEEE;" /></a>';
-$_['text_live']                      = 'Live';
-$_['text_sandbox']                   = 'Sandbox';
-$_['text_sale']                   = 'Sale';
+$_['text_extension']	= 'Extensions';                                         // OK  
+$_['text_payment']      = 'Payment';
+$_['text_success']      = 'Success: You have modified Netopia mobilPay account details!';
+$_['text_edit']         = 'Edit Netopia mobilPay';
+$_['text_mobilpay']     = '<a  onclick="window.open(\'https://netopia-payments.com\');">NETOPIA Payments</a>';    // OK
+$_['text_live']         = 'Live';
+$_['text_sandbox']      = 'Sandbox';
+$_['text_sale']         = 'Sale';
 
 // Entry
-$_['entry_signature']    				       = 'Account Signature';
-$_['entry_live_api_key']    				   = 'Live Api Key';
-$_['entry_sandbox_api_key']    				   = 'Sandbox Api Key';
-$_['entry_test']         				       = 'Sandbox mode';
-$_['entry_order_status'] 				       = 'Order Status Confirmed:';
+$_['entry_signature']    				       = 'Account Signature';               // OK
+$_['entry_live_api_key']    				   = 'Live Api Key';                    // OK
+$_['entry_sandbox_api_key']    				   = 'Sandbox Api Key';                 // OK
+$_['entry_test']         				       = 'Sandbox mode';                    // OK
+$_['entry_order_status'] 				       = 'Order Status Confirmed:';         // OK
 $_['entry_order_status_confirmed_pending']     = 'Order Status Confirmed Pending:';
 $_['entry_order_status_paid_pending'] 	       = 'Order Status Paid Pending:';
 $_['entry_order_status_paid']		 	       = 'Order Status Paid:';
 $_['entry_order_status_canceled']	 	       = 'Order Status Canceled:';
 $_['entry_order_status_credit']		 	       = 'Order Status Credit:';
-$_['entry_geo_zone']     				       = 'Geo Zone:';
-$_['entry_status']       				       = 'Status:';
-$_['entry_sort_order']   				       = 'Sort Order:';
+$_['entry_geo_zone']     				       = 'Geo Zone:';                       // OK
+$_['entry_status']       				       = 'Status:';                         // OK
+$_['entry_sort_order']   				       = 'Sort Order:';                     // OK
 $_['entry_total']                              = 'Total';
 
 // Help
-$_['help_total']					= 'The checkout total the order must reach before this payment method becomes active.';
+$_['help_total']            = 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment mobilPay!';
-$_['error_signature']    = 'Account Signature Required!'; 
+$_['error_permission']              = 'Warning: You do not have permission to modify payment mobilPay!';
+$_['error_signature']               = 'Account Signature Required!';
+$_['error_live_apikey']             = 'Live Api Key Required!';
+$_['error_sandbox_apikey']          = 'Sandbox Api Key Required!';
